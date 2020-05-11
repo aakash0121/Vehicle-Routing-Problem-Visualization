7d2acdaaafe2b9f2057ad9b76fd4fc3365512d1d
Aakash Bindal <bindalaakash60@gmail.com>
2020-05-11 14:25:52 +0530

    added map.py

---
+++ map.py
@@ -0,0 +1,39 @@
+import folium
+
+m = folium.Map(location=[41.2284, 80.9098], zoom_start=3)
+
+def drawMarkers(cordinates):
+    global m
+    for i in range(len(cordinates)):
+        
+        # lat = float(str(cordinates.lat))
+        # lon = float(str(cordinates.lon))
+
+        # drawing marker
+        folium.Marker(location=[cordinates[i].lat, cordinates[i].lon],popup=f'<strong>{i}</strong>',icon=folium.Icon(color='red')).add_to(m)
+
+    m.save('map.html')
+    return m
+
+# draw lines between two points
+def drawLines(route):
+    global m
+    for i in range(len(route)):
+        from_city = route[i]
+        if i + 1 < len(route):
+            to_city = route[i+1]
+        else:
+            to_city = route[0]
+
+        lat_from_city = from_city.lat
+        lon_from_city = from_city.lon
+
+        lat_to_city = to_city.lat
+        lon_to_city = to_city.lon
+
+        from_city = [lat_from_city, lon_from_city]
+        to_city = [lat_to_city, lon_to_city]
+
+        folium.PolyLine(locations=[from_city, to_city], color='blue').add_to(m)
+
+    m.save('map.html')
