40461287da8ff9d02741404968526354c7915654
aakash0121 <bindalaakash60@gmail.com>
2020-05-13 21:42:47 +0530

    change writeCsv

--- read_write_data.py
+++ read_write_data.py
@@ -1,45 +1,29 @@
 import csv
 import random
 
-
-FILE_NAME = 'data.csv'
-CITY_COUNT = 25
-
 # write co-ordinates in csv file
-def writeCsv(filename, city_count):
-    # fields = ['latitudes', 'longitudes']
-
+def writeCsv(filename, data):
     with open(filename, 'w') as f:
         csv_write = csv.writer(f)
 
-        # write headers
-        # csv_write.writerow(fields)
+        for i in range(len(data)):
+            latitude = data[i][0]
 
-        # write rows with co-ordinates choosen at random
-        for i in range(city_count):
-            # range of latitudes are -85 to 85
-            latitude = random.random()*(170) - 85
+            longitude = data[i][1]
 
-            # range of longitudes are -180 to 180
-            longitude = random.random()*(360) - 180
-
-            data = [latitude, longitude]
+            d = [latitude, longitude]
 
             # writing in csv
-            csv_write.writerow(data)
+            csv_write.writerow(d)
         
-def loadData():
-    global FILE_NAME
+def loadData(filename):
     # intializing list for storing co-ordinates of cities(or nodes)
     cityList = []
-
-    with open(FILE_NAME, 'r') as f:
+    
+    with open(filename, 'r') as f:
         csvreader = csv.reader(f)
 
         for row in csvreader:
             cityList.append(tuple(map(float, row)))
     
     return cityList
-
-if __name__ == "__main__":
-    writeCsv(FILE_NAME, CITY_COUNT)
