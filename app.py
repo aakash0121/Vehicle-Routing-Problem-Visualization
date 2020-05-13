be57de12e675926d10b313ab72341cc3f71e59ef
aakash0121 <bindalaakash60@gmail.com>
2020-05-13 21:41:08 +0530

    route change

--- app.py
+++ app.py
@@ -1,33 +1,50 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import folium
-from read_write_data import loadData
+from read_write_data import loadData, writeCsv
 import map
-from genetic_algo import execute_genetic
+import os
+from genetic_algo import execute_genetic, plot_distance_with_iterations
 
 app = Flask(__name__)
-# m.save('templates/map.html')
-
-# load the data
-data = loadData()
-
-m = folium.Map(location=[41.2284, 80.9098], zoom_start=3)
-
-map.initial_map(m, data)
-m.save('templates/map.html')
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
+    m = folium.Map(location=[41.2284, 80.9098], zoom_start=3)
+    m.add_child(folium.ClickForMarker())
+    m.save('templates/map.html')
+
     return render_template('map.html')
 
 @app.route('/progress', methods = ['GET', 'POST'])
 def final():
+
+    distance_list = []
+    cordinates = []
+    data = request.values.get('arr_data')
+    data = data.split(",")
+    for i, d in enumerate(data):
+        if i < len(data)-1:
+            lat = float(d[1:8])
+            lon = float(d[9:16])
+            cordinates.append([lat, lon])
+    
+    writeCsv('static/data.csv', cordinates)
+             
     popSize = int(request.form["popSize"])
     elitism = int(request.form["elitism"])
     mutationRate = float(request.form["mutationRate"])
     generations = int(request.form["generations"])
-    # print(popSize, elitism, mutationRate, generations)
-    execute_genetic(popSize=popSize, eliteSize=elitism, mutationRate=mutationRate, generations=generations)
-    return render_template('map.html')
+    for i in execute_genetic(popSize=popSize, eliteSize=elitism, mutationRate=mutationRate, generations=generations):
+        bestRoute = i[0]
+        distance = i[1]
+        distance_list.append(distance)
+        map.drawLines(bestRoute)
+    print(distance)
+    
+    plot_distance_with_iterations(distance_list)
+    os.remove("static/data.csv")
+    return render_template('map.html', distance=distance)
 
+    return jsonify({'status':'200'})
 if __name__ == "__main__":
     app.run(debug=True)
