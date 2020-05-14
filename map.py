cf78884ea18b8c94e211d3609d379c6a99a8fd2a
aakash0121 <bindalaakash60@gmail.com>
2020-05-14 16:47:08 +0530

    fixed icons

--- map.py
+++ map.py
@@ -5,20 +5,6 @@ from collections import namedtuple
 
 def get_bearing(p1, p2):
     
-    '''
-    Returns compass bearing from p1 to p2
-    
-    Parameters
-    p1 : namedtuple with lat lon
-    p2 : namedtuple with lat lon
-    
-    Return
-    compass bearing of type float
-    
-    Notes
-    Based on https://gist.github.com/jeromer/2005586
-    '''
-    
     long_diff = np.radians(p2.lon - p1.lon)
     
     lat1 = np.radians(p1.lat)
@@ -35,22 +21,7 @@ def get_bearing(p1, p2):
         return bearing + 360
     return bearing
 
-def get_arrows(locations, color='blue', size=6, n_arrows=3):
-    
-    '''
-    Get a list of correctly placed and rotated 
-    arrows/markers to be plotted
-    
-    Parameters
-    locations : list of lists of lat lons that represent the 
-                start and end of the line. 
-                eg [[41.1132, -96.1993],[41.3810, -95.8021]]
-    arrow_color : default is 'blue'
-    size : default is 6
-    n_arrows : number of arrows to create.  default is 3
-    Return
-    list of arrows/markers
-    '''
+def get_arrows(locations, color='red', size=6, n_arrows=3):
     
     Point = namedtuple('Point', field_names=['lat', 'lon'])
     
@@ -58,14 +29,8 @@ def get_arrows(locations, color='blue', size=6, n_arrows=3):
     p1 = Point(locations[0][0], locations[0][1])
     p2 = Point(locations[1][0], locations[1][1])
     
-    # getting the rotation needed for our marker.  
-    # Subtracting 90 to account for the marker's orientation
-    # of due East(get_bearing returns North)
     rotation = get_bearing(p1, p2) - 90
-    
-    # get an evenly space list of lats and lons for our arrows
-    # note that I'm discarding the first and last for aesthetics
-    # as I'm using markers to denote the start and end
+
     arrow_lats = np.linspace(p1.lat, p2.lat, n_arrows + 2)[1:n_arrows+1]
     arrow_lons = np.linspace(p1.lon, p2.lon, n_arrows + 2)[1:n_arrows+1]
     
@@ -84,9 +49,17 @@ def drawMarkers(cordinates):
 
     for i in range(len(cordinates)):
 
-        # drawing marker
-        folium.Marker(location=[cordinates[i].lat, cordinates[i].lon],popup=f'<strong>{i}</strong>',
-                        icon=folium.Icon(color='blue')).add_to(m)
+        if i == 0:
+            # ic = folium.Icon(color="red", icon="static/truck.png", prefix="fa")
+            ic = folium.features.CustomIcon('static/truck.png', icon_size=(50, 50))
+            # drawing marker
+            folium.Marker(location=[cordinates[i].lat, cordinates[i].lon],popup=f'<strong>{i}</strong>',
+                        icon=ic).add_to(m)        
+        else:
+            ic = folium.features.CustomIcon('static/city.png', icon_size=(32, 32))
+            # drawing marker
+            folium.Marker(location=[cordinates[i].lat, cordinates[i].lon],popup=f'<strong>{i}</strong>',
+                            icon=ic).add_to(m)
     return m
 
 # draw lines between two points
@@ -110,7 +83,7 @@ def drawLines(route):
         from_city = [lat_from_city, lon_from_city]
         to_city = [lat_to_city, lon_to_city]
 
-        folium.PolyLine(locations=[from_city, to_city], color='blue').add_to(m)
+        folium.PolyLine(locations=[from_city, to_city], color='red').add_to(m)
         arrows = get_arrows(locations=[from_city, to_city], n_arrows=3)
         for arrow in arrows:
             arrow.add_to(m)
